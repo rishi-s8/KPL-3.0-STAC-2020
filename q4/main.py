d2993030c959d1e9d89fb1d25cb19b1563ebb0b4
Rishi Sharma <33762894+rishi-s8@users.noreply.github.com>
2020-06-08 23:51:19 +0530

    Update Problem4

--- q4/main.py
+++ q4/main.py
@@ -1,6 +1,6 @@
 # all imports below
 
-from astroplan import Observer, FixedTarget
+from astroplan import Observer
 from astropy.time import Time
 from datetime import datetime
 import numpy as np
@@ -15,8 +15,8 @@ Any extra lines of code (if required)
 as helper for this function.
 """
 
-startobs = datetime(2020, 6, 8, 16, 48, 31) #replace it by the time when Saturn will be just visible
-endobs = datetime(2020, 6, 9, 3, 2, 17) #replace it by the time when Saturn is no longer visible from SAC terrace
+startobs = datetime(2020, 6, 9, 0, 9, 24) #replace it by the time when Saturn will be just visible
+endobs = datetime(2020, 6, 9, 6, 41, 27) #replace it by the time when Saturn is no longer visible from SAC terrace
 
 def findSaturn(obstime):
     '''
@@ -30,14 +30,13 @@ def findSaturn(obstime):
     '''
 
 
-    co=astropy.coordinates.get_body(body='saturn',time=Time(obstime),location=None)
     ti=Time(obstime)
     mandi=EarthLocation(lat=31.775461*u.deg, lon=76.98600769042969*u.deg,height=1000*u.m)
+    co=astropy.coordinates.get_body(body='saturn',time=ti,location=mandi)
     alt=co.transform_to(AltAz(obstime=ti,location=mandi))
-    source = Observer(location=mandi)
+    source = Observer(location=mandi, timezone='Asia/Kolkata')
     azm=alt.secz
 
-    saturn_set_time = source.target_set_time(Time(obstime), co, which="nearest")
-    saturn_rise_time = source.target_rise_time(Time(obstime), co, which="nearest")
-
-    return (source.astropy_time_to_datetime(saturn_rise_time), source.astropy_time_to_datetime(saturn_set_time))
+    saturn_set_time = source.target_set_time(ti, co, which="nearest", horizon=20*u.deg)
+    saturn_rise_time = source.target_rise_time(ti, co, which="nearest", horizon=20*u.deg)
+    return (float(alt.alt.value), float(azm.value))
