3853a56204323eb842bc9c3c0e0bb50a085f3d6f
Rishi Sharma <33762894+rishi-s8@users.noreply.github.com>
2020-06-08 21:16:51 +0530

    Problem 3

--- q3/main.py
+++ q3/main.py
@@ -1,4 +1,5 @@
 # all imports below
+import math
 
 """
 Any extra lines of code (if required)
@@ -10,9 +11,25 @@ def findDelay(dist):
 	Parameters
 	----------
 	dist : A `float`
-	
+
 	Returns
 	-------
 	A `float`
 	'''
-	return NotImplementedError
+	M = 5.974e24 	# Mass of Earth
+	c = 2.998e8  	# Speed of Light
+	r = 6357000  	# Radius of Earth
+	G = 6.674e-11	# Gravitational Constant
+
+	num = 1-(2*G*M)/((dist)*(c*c))
+	den = 1-(2*G*M)/(r*c*c)
+
+	num = math.sqrt(num)
+	den = math.sqrt(den)
+	res = num/den
+	res = res-1
+	# print(res)
+
+	T = dist/c
+	ans = res*T
+	return ans
